31c895c62e595323ef23f2d43485a2c29bc4f7ac
Andres Ariza <and.694@gmail.com>
2020-08-04 10:29:40 -0500

    Components part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
+import  Menu  from "./components/MenuComponent";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar dark color="primary">
+    <div>
+      <Navbar color="light">
         <div className="container">
           <NavbarBrand href="/">Ristorante con fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
