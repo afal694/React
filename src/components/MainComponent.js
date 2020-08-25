96077400ef55d2b22de377b4806faaead480fe09
Andres Ariza <and.694@gmail.com>
2020-08-24 19:24:15 -0500

    4 assgn. task 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Footer from './FooterComponent';
 import About from "./AboutComponent";
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {postComment, fetchDishes, fetchComments, fetchPromos} from '../Redux/ActionCreators';
+import {postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders} from '../Redux/ActionCreators';
 import {actions} from 'react-redux-form';
 import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
@@ -29,6 +29,9 @@ const mapDispatchToProps = dispatch => ({
   },
   fetchPromos: () => {
     dispatch(fetchPromos())
+  },
+  fecthLeaders: () => {
+    dispatch(fetchLeaders())
   }
 });
 
@@ -42,16 +45,29 @@ class Main extends React.Component {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
-
+    this.props.fecthLeaders();
   }
 
   render() {
     const HomePage = () => {
-      return (<Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} dishesLoading={this.props.dishes.isLoading} dishesErrMess={this.props.dishes.errMess} promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]} promosLoading={this.props.promotions.isLoading} promosErrMess={this.props.promotions.errMess} leader={this.props.leaders.filter((leader) => leader.featured)[0]}/>);
+      return (<Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+      dishesLoading={this.props.dishes.isLoading} dishesErrMess={this.props.dishes.errMess}
+      promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+      promosLoading={this.props.promotions.isLoading} promosErrMess={this.props.promotions.errMess}
+      leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+      leadersLoading={this.props.leaders.isLoading} leadersErrMess={this.props.leaders.errMess}/>);
     }
 
     const DishWithId = ({match}) => {
       return (<DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} commentsLoading={this.props.comments.isLoading} commentsErrMess={this.props.comments.errMess} comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} postComment={this.props.postComment}/>);
+    }
+
+    const AboutUs = () => {
+      return (
+        <About leaders={this.props.leaders.leaders}
+          isLoading={this.props.leaders.isLoading}
+          errMess={this.props.leaders.errMess} />
+      );
     }
 
     return (<div>
@@ -63,7 +79,7 @@ class Main extends React.Component {
             <Route exact="exact" path="/menu" component={() => <Menu dishes={this.props.dishes}/>}/>
             <Route path='/menu/:dishId' component={DishWithId}/>
             <Route exact="exact" path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
-            <Route exact="exact" path="/aboutus" component={() => <About leaders={this.props.leaders}/>}/>
+            <Route exact="exact" path="/aboutus" component={AboutUs}/>
             <Redirect to="/home"/>
           </Switch>
         </CSSTransition>
